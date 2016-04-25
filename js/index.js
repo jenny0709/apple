574e9c91a0bfa8a260a508a77eb77ce16cf07415
mhz520 <1874798077@qq.com>
2016-04-25 23:46:37 +0800

    修改完善

--- js/index.js
+++ js/index.js
@@ -4,10 +4,17 @@ $(function(){
     var $max_nav=$(".max-nav");
     var $header=$(".header");
     var $shop2=$(".shop2");
+    var $cha=$(".btn+a");
     $btn.click(function(){
-        //$btn.css("background","none").html("X").css({color:"#fff"});
-        //$shop2.css("display","none");
-        $max_nav.slideToggle(500).css({position:"relative",zIndex:999});
+        $btn.css("display","none");
+        $cha.css("display","block");
+        $max_nav.slideDown(500).css({position:"relative",zIndex:999});
+        $header.css("background-color","#000");
+    });
+    $cha.click(function(){
+        $cha.css("display","none");
+        $btn.css("display","block");
+        $max_nav.slideUp(500).css({position:"relative",zIndex:999});
         $header.css("background-color","#000");
     });
     //底部下拉菜单
@@ -23,13 +30,79 @@ $(function(){
     var $oneW=$imgs.width();
     var now=0;
     var next=0;
-    setInterval(function(){
+    var $btns=$(".btns li");
+    var $lbtn=$(".lbtn");
+    var $rbtn=$(".rbtn");
+    $imgs.css({left:"100%"}).eq(0).css({left:0});
+    var flag=true;
+    function move(){
+        if(!flag){
+            return;
+        }
+        flag=false;
         next++;
-        if(next==$imgs.length){
+        if(next==$imgs.length) {
             next=0;
         }
-
-    },3000)
-
+        $imgs.eq(next).css({left:"100%"},500);
+        $imgs.eq(now).animate({left:"-100%"},500);
+        $imgs.eq(next).animate({left:0},500,function(){
+            flag=true;
+        });
+        $btns.eq(now).removeClass("active");
+        $btns.eq(next).addClass("active");
+        now=next;
+    }
+    var t=setInterval(move,1500);
+    var $Box=$(".banner");
+    $Box.hover(function(ev){
+        ev.preventDefault();
+        clearInterval(t);
+    },function(){
+        t=setInterval(move,1500);
+    });
+    $lbtn.click(function(ev){
+        ev.preventDefault();
+        if(!flag){
+            return;
+        }
+        flag=false;
+        next--;
+        if(next==-1) {
+            next=$imgs.length-1;
+        }
+        $imgs.eq(next).css({left:"-100%"},500);
+        $imgs.eq(now).animate({left:"100%"},500);
+        $imgs.eq(next).animate({left:0},500,function(){
+            flag=true;
+        });
+        $btns.eq(now).removeClass("active");
+        $btns.eq(next).addClass("active");
+        now=next;
+    });
+    $rbtn.click(function(ev){
+        ev.preventDefault();
+        move();
+    });
+    $btns.click(function(ev){
+        ev.preventDefault();
+        if(now==$(this).index()||!flag){
+            return false;
+        }
+        flag=false;
+        if($(this).index()<now){
+            $imgs.eq($(this).index()).css({left:"-100%"});
+            $imgs.eq(now).animate({left:"100%"},500);
+        }else{
+            $imgs.eq($(this).index()).css({left:"100%"});
+            $imgs.eq(now).animate({left:"-100%"},500);
+        }
+        $imgs.eq($(this).index()).animate({left:0},500,function(){
+            flag=true;
+        });
+        $btns.eq(now).removeClass("active");
+        $btns.eq($(this).index()).addClass("active");
+        now=next=$(this).index();
+    });
 
 });
